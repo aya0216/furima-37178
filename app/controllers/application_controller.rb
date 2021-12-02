e9a4ecbc37827f4239eea2de31b572ddba3bf0f4
aya0216 <ayakawabata0216@gmail.com>
2021-12-02 16:59:19 +0900

    新規投稿機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  binding.pry
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
