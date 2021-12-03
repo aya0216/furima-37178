f61fb95b04796ee728efc8675348e2e905ab745e
aya0216 <ayakawabata0216@gmail.com>
2021-12-03 23:06:50 +0900

    ルーティングの設定変更

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      
+    end
+    it 'emailが空では登録できない' do
+      
+    end
+    it 'email' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+    it '' do
+      
+    end
+  end
+end
