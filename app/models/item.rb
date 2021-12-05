f4cbe8a79aa63a846013cf200e95d7850f5afb0c
aya0216 <ayakawabata0216@gmail.com>
2021-12-05 16:46:19 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :order
+  has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :ship_date
 
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :ship_date_id
+  end
 end
