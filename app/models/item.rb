3fff42efa5630117e0c14ea332f16b7bb76c8ce5
aya0216 <ayakawabata0216@gmail.com>
2021-12-05 23:03:12 +0900

    価格に対する正規表現の記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,7 +23,7 @@ class Item < ApplicationRecord
     validates :image
     validates :title
     validates :info
-    validates :price
+    validates :price, format: { with: /\A[0-9]+\z/}, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_wqual_to: 9_999_999 }
   end
 
 end
