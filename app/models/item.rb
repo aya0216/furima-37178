821acd74b33d464e0aeb7d700dabf94c148e0ddb
aya0216 <ayakawabata0216@gmail.com>
2021-12-06 14:47:01 +0900

    バリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,18 +12,15 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :category_id, numericality: { other_than: 1 }
-    validates :status_id, numericality: { other_than: 1 }
-    validates :postage_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :ship_date_id, numericality: { other_than: 1 }
-  end
-
-  with_options presence: true do
     validates :image
     validates :title
     validates :info
-    validates :price, format: { with: /\A[0-9]+\z/}, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_wqual_to: 9_999_999 }
+    validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+    validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
+    validates :postage_id, numericality: { other_than: 1 , message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+    validates :ship_date_id, numericality: { other_than: 1 , message: "can't be blank" }
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_wqual_to: 9_999_999 }
   end
 
 end
