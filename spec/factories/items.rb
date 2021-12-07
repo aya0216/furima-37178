217f780f7cabcbb929d524acb32dc8b532dc724d
aya0216 <ayakawabata0216@gmail.com>
2021-12-07 15:38:55 +0900

    RuboCop修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    title {"商品名"}
-    info {"説明"}
-    category_id {2}
-    status_id {2}
-    postage_id {2}
-    prefecture_id {2}
-    ship_date_id {2}
-    price {2000}
+    title { "商品名" }
+    info { "説明" }
+    category_id { 2 }
+    status_id { 2 }
+    postage_id { 2 }
+    prefecture_id { 2 }
+    ship_date_id { 2 }
+    price { 2000 }
 
     association :user
 
