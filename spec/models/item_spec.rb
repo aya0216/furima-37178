6ff995e8bf6d9b085d33b79f78bdae57ccd0ea26
aya0216 <ayakawabata0216@gmail.com>
2021-12-07 15:17:24 +0900

    itemモデル単体コードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -64,9 +64,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it 'priceが¥9,999,999以上では出品できない' do
-        @item.price = 10,000,000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it 'priceが全角数字では出品できない' do
         @item.price = '２０００'
