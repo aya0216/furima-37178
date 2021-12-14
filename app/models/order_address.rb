088eee95909e55e422b5848ef2df640cb4f6f80d
aya0216 <ayakawabata0216@gmail.com>
2021-12-14 16:07:36 +0900

    Rubocopの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,15 +4,15 @@ class OrderAddress
 
 
   with_options presence: true do
-    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :block
-    validates :phone, format: {with: /\A\d{10,11}\z/}
+    validates :phone, format: { with: /\A\d{10,11}\z/ }
     validates :item_id
     validates :user_id
     validates :token
   end
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
